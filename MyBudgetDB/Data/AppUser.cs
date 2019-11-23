8d948a8217ee4f53d1be9692e059266bcc69462d
Draper, Daniel <ddraper@dmacc.edu>
2019-11-23 17:46:51 -0600

    First commit

---
+++ MyBudgetDB/Data/AppUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Data
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(32)]
+        public string FirstName { get; set; }
+
+        [StringLength(32)]
+        public string LastName { get; set; }
+
+        [StringLength(32)]
+        public string City { get; set; }
+
+        [StringLength(32)]
+        public string State { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
