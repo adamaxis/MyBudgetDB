aead88594ff535d78d93ce196baae320b641142b
Leidy Russell <leidysemail@gmail.com>
2019-11-29 00:15:28 -0600

    added ApplicationUser to model

--- MyBudgetDB/Models/BudgetCommands/CreateBudgetCommand.cs
+++ MyBudgetDB/Models/BudgetCommands/CreateBudgetCommand.cs
@@ -5,13 +5,14 @@ namespace MyBudgetDB.Models.BudgetCommands
 {
     public class CreateBudgetCommand : EditBudgetBase
     {
-        public UserBudget ToBudget()
+        public UserBudget ToBudget(ApplicationUser createdBy)
         {
             return new UserBudget
             {
                 InitAmount = Amount,
                 CreationDate = DateAdded,
-                Expenses = Expenses?.Select(x => x.ToExpense()).ToList()
+                Expenses = Expenses?.Select(x => x.ToExpense()).ToList(),
+                UserId = createdBy.Id,
             };
         }
     }
