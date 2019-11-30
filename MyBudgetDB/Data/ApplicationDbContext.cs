0f4e8906a0c9951199990c15693320590d3eac47
Leidy Russell <leidysemail@gmail.com>
2019-11-30 13:27:46 -0600

    removed Onconfiguring() with user secret settings

--- MyBudgetDB/Data/ApplicationDbContext.cs
+++ MyBudgetDB/Data/ApplicationDbContext.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 namespace MyBudgetDB.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        private readonly AppSecrets _DbInfo;
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IOptions<AppSecrets> DbInfo)
-            : base(options)
-        {
-            _DbInfo = DbInfo.Value ?? throw new ArgumentException(nameof(DbInfo));
-        }
-
-        // Added for unit test
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
@@ -30,8 +20,5 @@ namespace MyBudgetDB.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options) => 
-            options.UseSqlServer($"{_DbInfo.Database};User ID={_DbInfo.User};Password={_DbInfo.Password};{_DbInfo.Options};");
     }
 }
