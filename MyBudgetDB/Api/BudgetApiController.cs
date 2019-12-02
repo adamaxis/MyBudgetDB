cb474237a3c533112b3eb6fcb3d6b66dd3285333
Leidy Russell <leidysemail@gmail.com>
2019-12-01 22:40:04 -0600

    added more to api

--- MyBudgetDB/Api/BudgetApiController.cs
+++ MyBudgetDB/Api/BudgetApiController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyBudgetDB.Attributes;
+using MyBudgetDB.Data;
+using MyBudgetDB.Models;
 using MyBudgetDB.Models.BudgetCommands;
 using MyBudgetDB.Services;
 
@@ -12,10 +11,13 @@ namespace MyBudgetDB.Api
 {
     [Produces("application/json")]
     [Route("api/BudgetApi")]
-    //[ValidateModel, HandleException, FeatureEnabled(IsEnabled = true)]
+    [ValidateModel, HandleException, FeatureEnabled(IsEnabled = true)]
+    [ServiceFilter(typeof(HandleExceptionAttribute))]
+    [Authorize]
     public class BudgetApiController : Controller
     {
         public BudgetService _budgetService;
+        public UserService _userService;
         private readonly ILogger _log;
 
         public BudgetApiController(BudgetService service, ILogger<BudgetApiController> log)
@@ -24,23 +26,7 @@ namespace MyBudgetDB.Api
             _log = log;
         }
 
-        // POST: api/BudgetApi
-        //[HttpPost]
-        //public IActionResult Post([FromBody]CreateBudgetCommand value)
-        //{
-        //    var appUser = await _userService.GetUserAsync(User);
-        //    var id = _service.CreateBudget(value, appUser);
-
-        //    return Ok(new { id = id });
-        //}
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
-        [HttpGet("{id}")]
+        [HttpGet("{id}"), EnsureBudgetExist, AddLastModifiedHeader]
         public IActionResult Get(int id)
         {
             var detail = _budgetService.GetBudgetDetail(id);
@@ -48,10 +34,36 @@ namespace MyBudgetDB.Api
 
         }
 
-        [HttpPost("{id}")]// EnsureRecipeExists, RequireIpAddress
+        [HttpPost("{id}/{userId}/{userName}"),ValidateModel, EnsureBudgetExist, ValidateUser]
         public IActionResult Edit(int id, [FromBody] UpdateBudgetCommand command)
         {
             _budgetService.UpdateBudget(command);
+            return Ok();
+        }
+
+        // POST: api/BudgetApi
+        [HttpPost("create"), ValidateModel]
+        public IActionResult Create([FromBody]CreateBudgetCommand value, ApplicationUser createdBy)
+        {
+            var id = _budgetService.CreateBudget(value, createdBy);
+
+            return Ok(new { id = id });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]CreateUser user)
+        {
+            var id = _userService.CreateUser(user);
+            return Ok();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("delete /{id}"), ValidateModel, EnsureBudgetExist, AddLastModifiedHeader]
+        public IActionResult Delete(int id)
+        {
+            _budgetService.DeleteBudget(id);
+
             return Ok();
         }
     }
