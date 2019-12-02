1a0d546d12fc919beff8982569cafc41b58c6ef2
Leidy Russell <leidysemail@gmail.com>
2019-12-01 22:45:46 -0600

    added attributes

---
+++ MyBudgetDB/Attributes/AddLastModifiedHeader.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyBudgetDB.Models.BudgetCommands;
+
+namespace MyBudgetDB.Attributes
+{
+    public class AddLastModifiedHeader : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.Result is OkObjectResult result
+                && result.Value is UserBudgetDetails detail)
+            {
+                var lastModified = context.HttpContext.Request.GetTypedHeaders().IfModifiedSince;
+
+                if (lastModified.HasValue && lastModified >= detail.LastModified)
+                {
+                    context.Result = new StatusCodeResult(304);
+                }
+            }
+        }
+    }
+}
---
+++ MyBudgetDB/Attributes/FeatureEnabledAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MyBudgetDB.Attributes
+{
+    public class FeatureEnabledAttribute : Attribute, IResourceFilter
+    {
+        public bool IsEnabled { get; set; }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            // Nothing to do here
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            if (!IsEnabled)
+            {
+                context.Result = new BadRequestResult();
+            }
+        }
+    }
+}
