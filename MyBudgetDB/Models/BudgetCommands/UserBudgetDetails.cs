f3597157c88698470ed544a58e6f3cce88f7f1d5
Draper, Daniel <ddraper@dmacc.edu>
2019-12-04 14:30:28 -0600

    Few small changes

--- MyBudgetDB/Models/BudgetCommands/UserBudgetDetails.cs
+++ MyBudgetDB/Models/BudgetCommands/UserBudgetDetails.cs
@@ -8,13 +8,13 @@ namespace MyBudgetDB.Models.BudgetCommands
     {
         public int Id { get; set; }
         public string Owner { get; set; }
+        [Display(Name = "Budget Name")]
         public string Name { get; set; }
         [Display(Name = "Budget Amount"), DisplayFormat(DataFormatString = "{0:C}")]
         public double Amount { get; set; }
         [Display(Name = "Current Balance"), DisplayFormat(DataFormatString = "{0:C}")]
         public double Balance { get; set; }
         public DateTime CreationDate { get; set; }
-        public bool IsDeleted { get; set; }
         public DateTime LastModified { get; set; }
         public bool CanEditBudget { get; set; }
         public IEnumerable<Item> Expenses { get; set; }
@@ -24,6 +24,8 @@ namespace MyBudgetDB.Models.BudgetCommands
             public string Name { get; set; }
             [Display(Name = "Expense Cost"), DisplayFormat(DataFormatString = "{0:C}")]
             public double Amount { get; set; }
+            [Display(Name = "Notes on Expense")]
+            public string Notes { get; set; }
             public DateTime? DateAdded { get; set; }
         }
 
